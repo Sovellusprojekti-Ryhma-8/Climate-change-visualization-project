3b84555664a5d46e679a8799c1d02a8198b2673d
JonneM <jonne.mustajarvi@gmail.com>
2022-11-15 13:51:54 +0200

    rekisteröinti backend valmis

--- Java/project/src/main/java/com/climate_rest/project/data/User.java
+++ Java/project/src/main/java/com/climate_rest/project/data/User.java
@@ -1,16 +1,37 @@
 package com.climate_rest.project.data;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
 
 
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
 
+    @Column(name="username")
     private String userName;
+    @Column(name="password") 
     private String passWord;
+
+
+    public User() {
+
+    }
 
     public User(String userName, String passWord) {
         this.userName = userName;
         this.passWord = passWord;
     }
 
+    public int getId() {
+        return this.user_id;
+    }
 
     public String getUserName() {
         return this.userName;
