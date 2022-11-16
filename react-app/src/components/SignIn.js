0a69703721510388e69be6362de0dc60d1ac4b41
JonneM <jonne.mustajarvi@gmail.com>
2022-11-16 14:22:34 +0200

    registeration style muutettu

--- react-app/src/components/SignIn.js
+++ react-app/src/components/SignIn.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import '../styles/registeration.css'
 
 
@@ -7,9 +8,22 @@ const URL = "http://localhost:8080/register"
 
 export default function SignIn() {
 
-    const [userName, setUserame] = useState('');
+
+    const [userName, setUsername] = useState('');
     const [passWord, setPassWord] = useState('');
+    const [passWord2, setPassWord2] = useState('');
+    const [message, setMessage] = useState('');
     const form = new FormData;
+    let navigate = useNavigate();
+
+
+    useEffect(() => {
+        if (passWord === passWord2) {
+            console.log("yeet");
+        }else {
+            console.log("nooo")
+        }
+    })
 
 
     let handleSubmit = async (e) => {
@@ -23,10 +37,21 @@ export default function SignIn() {
                 method: 'POST',
                 body: form
             })
-            .then(res => console.log(res))
-            .catch(error => console.log(error))
+            .then((res) => {
+                if (res.status === 200) {
+                    console.log(res.status)
+                    navigate('/LogIn')
+                }else {
+                    setMessage("Vituiks män")
+                }
+            }) 
+            .catch((error) => {
+                console.log(error+ "  error")
+                setMessage("error")
+            })
         } catch(err) {
-            console.log(err);
+            console.log(err+ "   err");
+            setMessage("err")
         }
        
     }
@@ -38,10 +63,21 @@ export default function SignIn() {
         <>
             <div class="register">
                 <form class="register-form" onSubmit={handleSubmit}>
-                    <input type="text" value={userName} onChange={(e) => setUserame(e.target.value)}/>
-                    <input type="text" value={passWord} onChange={(e) => setPassWord(e.target.value)}/>
+                    <div class="block">
+                        <label>Käyttäjänimi</label>
+                        <input class="input" type="text" value={userName} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div class="block">
+                        <label>Salasana</label>
+                        <input class="input" type="text" value={passWord} onChange={(e) => setPassWord(e.target.value)}/>
+                    </div>
+                    <div class="block">
+                        <label>Salasana uudelleen</label>
+                        <input class="input" type="text" value={passWord2} onChange={(e) => setPassWord2(e.target.value)}/>
+                    </div>
                     <button class='button' type='submit'>Rekisteröidy</button>
                 </form>
+                <p>{message}</p>
             </div>
         </>
     )
