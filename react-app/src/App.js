f6e945ebae9970a244afafa00cd61fb4c6fd279c
nismq <83830065+nismq@users.noreply.github.com>
2022-11-23 11:40:24 +0200

    Update App.js
    Vaihoin divistä classname => class et toimii css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -20,7 +20,7 @@ function App() {
   
     <Navbar />
     <Header />
-    <div classname='container'>
+    <div class='container'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/N1" element={<N1 />} />
