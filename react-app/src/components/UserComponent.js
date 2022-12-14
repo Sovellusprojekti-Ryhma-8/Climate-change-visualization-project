51d7302deccd7016fc57bdaae7892da49fa7371f
JonneM <jonne.mustajarvi@gmail.com>
2022-12-14 13:54:47 +0200

    tyyli valinnat toimii

--- react-app/src/components/UserComponent.js
+++ react-app/src/components/UserComponent.js
@@ -23,6 +23,7 @@ const UserComponent = (props) => {
     const [counter, setCounter] = useState(10);
     const [visuals, setVisuals] = useState([]);
     const [desc, setDesc] = useState([]);
+    const [style, setStyle] = useState(0);
     const Id = useParams()
     const form = new FormData;
     
@@ -39,6 +40,7 @@ const UserComponent = (props) => {
             setData(res.data)
             setVisuals(res.data.visualizations)
             setDesc(res.data.descriptions)
+            setStyle(res.data.style)
             setCounter(1)                
         }).catch(error => {
             alert(error)
@@ -53,79 +55,139 @@ const UserComponent = (props) => {
             switch (element) {
                 case "v1":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V1 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                         setComp(
-                            components => [...components, <V1 key={components.lenght} text={desc[index]}/>]
+                            components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V1 key={components.lenght} text={desc[index]}/></li>]
                         )
+                        }
                     }
                     break;
                 case "v2":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V2 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                     setComp(
-                        components => [...components, <V2 key={components.lenght} text={desc[index]}/>]
+                        components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V2 key={components.lenght} text={desc[index]}/></li>]
                     )
-                    }
+                    }}
                     break;
                 case "v3":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V3 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                     setComp(
-                        components => [...components, <V3 key={components.lenght} text={desc[index]}/>]
+                        components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V3 key={components.lenght} text={desc[index]}/></li>]
                     )
-                    }
+                    }}
                     break;
                 case "v4":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V4 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                     setComp(
-                        components => [...components, <V4 key={components.lenght} text={desc[index]}/>]
+                        components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V4 key={components.lenght} text={desc[index]}/></li>]
                     )
-                    }
+                    }}
                     break;
                 case "v5":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V5 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                     setComp(
-                        components => [...components, <V5 key={components.lenght} text={desc[index]}/>]
+                        components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V5 key={components.lenght} text={desc[index]}/></li>]
                     )
-                    }                    
+                    }}                 
                     break;
                 case "v6":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V6 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                     setComp(
-                        components => [...components, <V6 key={components.lenght} text={desc[index]}/>]
+                        components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V6 key={components.lenght} text={desc[index]}/></li>]
                     )
-                    }
+                    }}
                     break;
                 case "v7":   
-                if (visuals.length > components.length) {                 
+                if (visuals.length > components.length) { 
+                    if (style == 1) {
+                        setComp(
+                            components => [...components,<li> <V7 key={components.lenght} text={desc[index]}/></li>]
+                        )
+                    }else{                
                     setComp(
-                        components => [...components, <V7 key={components.lenght} text={desc[index]}/>]
+                        components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V7 key={components.lenght} text={desc[index]}/></li>]
                     )
-                }
+                }}
                     break; 
                 case "v8":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V8 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                     setComp(
-                        components => [...components, <V8 key={components.lenght} text={desc[index]}/>]
+                        components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V8 key={components.lenght} text={desc[index]}/></li>]
                     )
-                    }
+                    }}
                     break;
                 case "v9":
                     if (visuals.length > components.length) {
+                        if (style == 1) {
+                            setComp(
+                                components => [...components,<li> <V9 key={components.lenght} text={desc[index]}/></li>]
+                            )
+                        }else{
                         setComp(
-                            components => [...components, <V9 key={components.length} text={desc[index]}/>]
+                            components => [...components,<li style={{display: "inline-block", width: "47%"}}> <V9 key={components.length} text={desc[index]}/></li>]
                         )
-                    }
+                    }}
                     break;           
                 default:
                     break;
             }
         });
+
+        if (style == 1) {
+            const element = document.querySelector('.content')  
+            element.style.listStyle = "none"         
+        }else if (style == 2) {
+            const element = document.querySelector('.content')
+            element.style.display = "inline"
+            element.style.listStyle = "none"
+        }
+
+
         console.log(visuals)
+        console.log(style)
         setCounter(counter+1)    
     }
     
     return (
-        <>
+ 
+        <ul class="content">
         {components}
-        </>
+        </ul>
+
     )
 }
 export default UserComponent;
